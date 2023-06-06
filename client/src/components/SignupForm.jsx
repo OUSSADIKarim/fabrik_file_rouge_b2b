8556a55d401a2715f10feb222a2c46e1f79016f1
OUSSADI Karim <oussadikarim00@gmial.com>
2023-06-06 17:14:42 +0100

    client added

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -7,74 +7,21 @@ const SignupForm = () => {
 
   return (
     <form className="w-full flex items-center justify-center">
-      <div className="w-full h-full flex flex-col items-center justify-center gap-4">
-        <Input id="email" type="email" placeholder="Email" className="w-1/2" />
-        <Input
-          id="name"
-          type="text"
-          placeholder="Company Name"
-          className="w-1/2"
-        />
-        <Input
-          id="companySize"
-          type="text"
-          placeholder="Company Size"
-          className="w-1/2"
-        />
-        <Input
-          id="employeesNumber"
-          type="text"
-          placeholder="employeesNumber"
-          className="w-1/2"
-        />
-        <Input
-          id="legalStatus"
-          type="text"
-          placeholder="legalStatus"
-          className="w-1/2"
-        />
-        <Input
-          id="socialCapital"
-          type="text"
-          placeholder="socialCapital"
-          className="w-1/2"
-        />
-        <Input
-          id="headquarter"
-          type="text"
-          placeholder="headquarter"
-          className="w-1/2"
-        />
-      </div>
-      <div className="w-full h-full flex flex-col items-center justify-center gap-4">
-        <Input
-          id="website"
-          type="url"
-          placeholder="website"
-          className="w-1/2"
-        />
-        <Textarea
-          id="description"
-          type="text"
-          placeholder="description"
-          className="w-1/2"
-        />
-        <Input
-          id="phoneNumber"
-          type="text"
-          placeholder="phoneNumber"
-          className="w-1/2"
-        />
-        <Input id="nrc" type="text" placeholder="nrc" className="w-1/2" />
-        <Input id="nif" type="text" placeholder="nif" className="w-1/2" />
-        <Input
-          id="businessSectors"
-          type="text"
-          placeholder="businessSectors"
-          className="w-1/2"
-        />
-        <Input id="logo" type="file" placeholder="logo" className="w-1/2" />
-      </div>
+      <Input
+        id="name"
+        type="text"
+        placeholder="Company Name"
+        className="w-1/2"
+      />
+      <Input id="nrc" type="text" placeholder="nrc" className="w-1/2" />
+      <Input id="nif" type="text" placeholder="nif" className="w-1/2" />
+      <Input id="email" type="email" placeholder="Email" className="w-1/2" />
+      <Input
+        id="password"
+        type="password"
+        placeholder="password"
+        className="w-1/2"
+      />
       <Button onClick={handleSignup}>Signup</Button>
     </form>
   )
